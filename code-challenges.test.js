364c47274092aaa87d78d7caae3be6528436175a
ios10 <trikshotzz05@gmail.com>
2022-06-20 23:25:28 -0700

    more

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -51,37 +51,30 @@
 
 // b) Create the function that makes the test pass.
 
-// input:const people = [
-        // { name: "ford prefect", occupation: "a hitchhiker" },
-        // { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-        // { name: "arthur dent", occupation: "a radio employee" }
+// input: const people = [
+//         { name: "ford prefect", occupation: "a hitchhiker" },
+//         { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+//         { name: "arthur dent", occupation: "a radio employee" }]
 // create a function called join
 // create an empty array 
 // the function will iterate through the array
 // return each element with string interpolation 
 // output:["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
+
+  const joined = (join) => {
+    let arr = join.map(value => `${value.name} is ${value.occupation}`
+     )
+     let sentence = arr.map(value => value.charAt(0).toUpperCase() + value.substring(1)
+     )
+        return sentence
+       }
+
   // const joined = (join) => {
-  //   let arr = `${this.name} is ${this.occupation}`
-  //   return arr
-    // for(let i =0; i < join.length; i++){
-    //    join[i] = `${join.name} is ${join.occupation}`
-
+  //   let {name, occupation} = join 
+  //   let sentence = `${join.name} is ${join.occupation}`
+  //   return sentence
   // }
-
-  // const joined = (join) => {
-  //   let arr = join.map(value => {
-  //    return `${value.name} is ${value.occupation}`
-  //    })
-  //    const str = arr.join(" ")
-  //    const upper = str.charAt(0).toUpperCase() + str.substring(1)
-  //    const upp = upper.split(" ")
-    
-  //     return upp
-      
-  // }
-
-
 
 
 
@@ -96,14 +89,14 @@
 
   // create a describe it that expects the output of and array with remainder numbers
 
-    describe('num', () => { 
-      it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
-        const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-        const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-        expect(num(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
-        expect(num(hodgepodge2)).toEqual([ 2, 1, -1 ])
-      })
-    })
+    // describe('num', () => { 
+    //   it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+    //     const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+    //     const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+    //     expect(num(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    //     expect(num(hodgepodge2)).toEqual([ 2, 1, -1 ])
+    //   })
+    // })
 
 
     // I failed 
@@ -131,23 +124,27 @@
 // output:[ 2, 0, -1, 0 ]
         // [ 2, 1, -1 ]
 
-          const num = (numbers) => {
-            let ha = []
-            let arr = numbers.filter(value =>{
-              if(value === 'number'){
-                  return value % 3
-              }
-            })
-            return arr 
-          }
+          // const num = (numbers) => {
+           
+          //   let arr = numbers.filter(value => typeof value === 'number')
+          //   let round = arr.map(value => value % 3)
+          //     return round
+            
+          // }
 
-          // - Array [
-          //   -   2,
-          //   -   0,
-          //   -   -1,
-          //   -   0,
-          //   - ]
-          //   + Array [] . idk where this empty array is coming from 
+          
+
+        //   I passed 
+        //   PASS  ./code-challenges.test.js
+        //   num
+        //     ✓ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3 (3 ms)
+        
+        // Test Suites: 1 passed, 1 total
+        // Tests:       1 passed, 1 total
+        // Snapshots:   0 total
+        // Time:        0.306 s, estimated 1 s
+        // Ran all test suites.
+        // ✨  Done in 1.15s.
 
 
 
@@ -160,15 +157,15 @@
 
           // create a describe it with the expect of all the numbers in the array summed after being cubed 
 
-          describe("cubed", () => {
-            it("takes in an array of numbers and returns the sum of all the numbers cubed", () => {
-              const cubeAndSum1 = [2, 3, 4]
-              const cubeAndSum2 = [0, 5, 10]
-              expect(cubed(cubeAndSum1)).toEqual(99)
-              expect(cubed(cubeAndSum2)).toEqual(1125)
+          // describe("cubed", () => {
+          //   it("takes in an array of numbers and returns the sum of all the numbers cubed", () => {
+          //     const cubeAndSum1 = [2, 3, 4]
+          //     const cubeAndSum2 = [0, 5, 10]
+          //     expect(cubed(cubeAndSum1)).toEqual(99)
+          //     expect(cubed(cubeAndSum2)).toEqual(1125)
 
-            })
-          })
+          //   })
+          // })
 
 
           // I failed 
@@ -194,13 +191,13 @@
     // the function will 
 
 
-          const cubed = (cube) => {
-            let sum = 0
-            for(let i = 0; i < cube.length; i++){
-              sum += cube[i] * cube[i] * cube[i] 
-            }
-            return sum
-          }
+          // const cubed = (cube) => {
+          //   let sum = 0
+          //   for(let i = 0; i < cube.length; i++){
+          //     sum += cube[i] * cube[i] * cube[i] 
+          //   }
+          //   return sum
+          // }
 
           // I passed 
         //   PASS  ./code-challenges.test.js
